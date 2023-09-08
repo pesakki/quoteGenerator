1a0a26ab278f65f7f1cfcb5e1f31a7219dc34f87
Esakkiammal <spesakki1@gmail.com>
2023-09-08 13:24:40 +0530

    tester include

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,8 @@
-const { getQuote, getRandomQuote } = require("../lib/index");
+const { getRandomQuote } = require("../lib/index");
 
 console.log("Random Quote:");
 console.log(getRandomQuote());
 
-console.log("dcsfv s",getSuccessQuote().text);
+console.log("dcsfv s",getSuccessQuote());
 
 console.log(getSuccessQuote({ author: false }));
---
+++ tester/app.js
@@ -0,0 +1,4 @@
+let a = require("team-success-quotes");
+
+console.log("Random Quote:");
+console.log(a.getRandomQuote());
